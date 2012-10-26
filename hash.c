ca1324919a1dce35c36235b31c9f8ca63c2eb764
swiecki <daniel.swiecki@gmail.com>
2012-10-26 19:52:30 -0400

    added size function

--- hash.c
+++ hash.c
@@ -3,9 +3,43 @@
 
 // create a new hashtable; parameter is a size hint
 hashtable_t *hashtable_new(int sizehint) {
-    return NULL;
+    int size = hashtable_size_calculator(sizehint);
+		return NULL;
 }
-
+int hashtable_size_calculator(int sizehint){
+	if(sizehint < 37){
+		return 37;
+	}
+	else if (sizehint < 71){
+		return 71;
+	}
+	else if (sizehint < 173){
+		return 173;
+	}
+	else if (sizehint < 281){
+		return 281;
+	}
+	else if (sizehint < 409){
+		return 409;
+	}
+	else if (sizehint < 541){
+		return 541;
+	}
+	else if (sizehint < 659){
+		return 659;
+	}
+	else if (sizehint < 809){
+		return 809;
+	} 
+	else if (sizehint < 941){
+		return 941;
+	}
+	else if (sizehint < 1069){
+		return 1069;
+	} else {
+		return sizehint;
+	}
+}
 // free anything allocated by the hashtable library
 void hashtable_free(hashtable_t *hashtable) {
 
