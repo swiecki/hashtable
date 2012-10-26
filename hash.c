656d806f7fb5f1e6cee86a0f6690f1f4fbe44455
swiecki <daniel.swiecki@gmail.com>
2012-10-26 19:37:59 -0400

    added hash function

--- hash.c
+++ hash.c
@@ -14,6 +14,14 @@ void hashtable_free(hashtable_t *hashtable) {
 // add a new string to the hashtable
 void hashtable_add(hashtable_t *hashtable, const char *s) {
 
+	//generate a key for our array based off the string
+	int key = 0;
+  	while (*s) {
+    	key = key*37 + *s++;
+  	}
+	key = key % hashtable->size;
+	//put the string into the bucket
+
 }
 
 // remove a string from the hashtable; if the string
