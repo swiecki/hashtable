573a51a15a93b5bbdd9fb338970313c727f24f64
swiecki <daniel.swiecki@gmail.com>
2012-10-26 18:50:41 -0400

    first commit

---
+++ Makefile
@@ -0,0 +1,21 @@
+CC = gcc
+CFLAGS = -g -Wall -pthread
+# main executable file first
+TARGET = mthash
+# object files next
+OBJS = main.o hash.o
+# header files next
+DEPS = hash.h
+.PHONY : clean
+
+all: $(TARGET)
+
+$(TARGET): $(OBJS)
+	$(CC) $(CFLAGS) -o $@ $(OBJS) -pthread
+
+.c.o: $(DEPS)
+	$(CC) $(CFLAGS) -c $<
+
+clean:
+	rm -f $(OBJS) $(TARGET) *~
+
---
+++ hash.c
@@ -0,0 +1,30 @@
+#include "hash.h"
+
+
+// create a new hashtable; parameter is a size hint
+hashtable_t *hashtable_new(int sizehint) {
+    return NULL;
+}
+
+// free anything allocated by the hashtable library
+void hashtable_free(hashtable_t *hashtable) {
+
+}
+
+// add a new string to the hashtable
+void hashtable_add(hashtable_t *hashtable, const char *s) {
+
+}
+
+// remove a string from the hashtable; if the string
+// doesn't exist in the hashtable, do nothing
+void hashtable_remove(hashtable_t *hashtable, const char *s) {
+
+}
+
+// print the contents of the hashtable
+void hashtable_print(hashtable_t *hashtable) {
+
+}
+
+
